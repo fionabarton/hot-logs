c7cd32816de60ba798e37b39340cc63ab75be08b
fionabarton <56236345+fionabarton@users.noreply.github.com>
2021-10-03 10:30:37 -0400

    Add SpriteSwapOnEnable.cs
    SpriteSwapOnEnable.cs:
    - Remove half of the contents of SpriteSwap.cs and implement them in this new script.

---
+++ _Scripts/Triggers/SpriteSwapOnEnable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteSwapOnEnable : MonoBehaviour {
+	[Header("Set in Inspector")]
+	public List<Sprite>		sprites = new List<Sprite>();
+
+	public SpriteRenderer	sRend;
+
+	void OnEnable() {
+		// Change Parallax Background
+		if (RainSpawner.S.isRaining) {
+			sRend.sprite = sprites[0];
+		} else {
+			sRend.sprite = sprites[1];
+		}
+	}
+}
