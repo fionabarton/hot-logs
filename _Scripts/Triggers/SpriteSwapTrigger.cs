e22276732f322d49361e6fee82c46d744d5e0cf9
fionabarton <56236345+fionabarton@users.noreply.github.com>
2021-10-04 17:16:00 -0400

    Add SpriteSwapTrigger.cs
    SpriteSwapTrigger.cs:
    - Remove half of the contents of SpriteSwap.cs and implement them in this new script.
    - Add the option to play a sound OnTriggerEnter (ex. opening doors).

---
+++ _Scripts/Triggers/SpriteSwapTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteSwapTrigger : MonoBehaviour {
+	[Header("Set in Inspector")]
+	public List<Sprite>		sprites = new List<Sprite>();
+
+	public bool				swapOnlyOnFirstCollision = true;
+	public int				soundNdx;
+
+	public SpriteRenderer	sRend;
+
+	void OnTriggerEnter2D(Collider2D coll) {
+		if (swapOnlyOnFirstCollision) {
+			if (coll.gameObject.CompareTag("Player")) {
+				sRend.sprite = sprites[0];
+
+				AudioManager.S.PlaySFX(soundNdx);
+
+				swapOnlyOnFirstCollision = false;
+			}
+		}
+	}
+}
