0749e58639f42df51cd313254722831dc2f74911
fionabarton <56236345+fionabarton@users.noreply.github.com>
2021-10-20 18:13:06 -0400

    AudioManager.cs: add previousSongNdx

--- _Scripts/Managers/AudioManager.cs
+++ _Scripts/Managers/AudioManager.cs
@@ -12,9 +12,10 @@ public class AudioManager : MonoBehaviour {
 	public List <AudioSource>	sfxCS = new List<AudioSource>();
 
 	[Header("Set Dynamically")]
+	public int					previousSongNdx;
 	public int 					currentSongNdx;
 
-	public AudioListener		audioListenerCS;
+	public float				previousVolumeLvl;
 
 	// Singleton
 	private static AudioManager _S;
@@ -25,8 +26,6 @@ public class AudioManager : MonoBehaviour {
 	}
 
 	void Start() {
-		audioListenerCS = GetComponent<AudioListener>();
-
 		// Add Loop() to UpdateManager
 		UpdateManager.updateDelegate += Loop;
 	}
@@ -37,22 +36,11 @@ public class AudioManager : MonoBehaviour {
 		}
 	}
 
-	public void PlaySong(int ndx = 0){
-		// Change index
-		currentSongNdx = ndx;
-
-		// Stop ALL BGM
-		for (int i = 0; i < bgmCS.Count; i++) {
-			bgmCS[i].Stop();
-		}
-
-		if (!AudioListener.pause) {
-			bgmCS[ndx].Play();
-		}
-	}
-
 	public void PlaySong(eSongName songName) {
-		// Change index
+		// Set previous song index
+		previousSongNdx = currentSongNdx;
+
+		// Set current song index
 		currentSongNdx = (int)songName;
 
 		// Stop ALL BGM
@@ -61,41 +49,25 @@ public class AudioManager : MonoBehaviour {
 		}
 
 		switch (songName) {
-			case eSongName.nineteenForty:
-				bgmCS[0].Play();
-				break;
-			case eSongName.never:
-				bgmCS[1].Play();
-				break;
-			case eSongName.ninja:
-				bgmCS[2].Play();
-				break;
-			case eSongName.soap:
-				bgmCS[3].Play();
-				break;
-			case eSongName.things:
-				bgmCS[4].Play();
-				break;
-			case eSongName.startBattle:
-				bgmCS[5].Play();
-				break;
-			case eSongName.win:
-				bgmCS[6].Play();
-				break;
-			case eSongName.lose:
-				bgmCS[7].Play();
-				break;
+			case eSongName.nineteenForty: bgmCS[0].Play(); break;
+			case eSongName.never: bgmCS[1].Play(); break;
+			case eSongName.ninja: bgmCS[2].Play(); break;
+			case eSongName.soap: bgmCS[3].Play(); break;
+			case eSongName.things: bgmCS[4].Play(); break;
+			case eSongName.startBattle: bgmCS[5].Play(); break;
+			case eSongName.win: bgmCS[6].Play(); break;
+			case eSongName.lose: bgmCS[7].Play(); break;
 		}
 	}
 
 	public void PauseMuteSong(){
 		if (!AudioListener.pause) {
-			AudioListener.volume = 0;
+			previousVolumeLvl = AudioListener.volume;
 			AudioListener.pause = true;
 
 			bgmCS[currentSongNdx].Pause();
 		} else {
-			AudioListener.volume = 1;
+			AudioListener.volume = previousVolumeLvl;
 			AudioListener.pause = false;
 
 			bgmCS[currentSongNdx].Play();
@@ -105,54 +77,39 @@ public class AudioManager : MonoBehaviour {
 	public void PlaySFX(int ndx) {
  		sfxCS[ndx].Play();
     }
-
 	public void PlaySFX(eSoundName soundName) {
         switch (soundName) {
-			case eSoundName.dialogue:
-				sfxCS[0].Play();
-				break;
-			case eSoundName.selection:
-				sfxCS[1].Play();
-				break;
-			case eSoundName.damage1:
-				sfxCS[2].Play();
-				break;
-			case eSoundName.damage2:
-				sfxCS[3].Play();
-				break;
-			case eSoundName.damage3:
-				sfxCS[4].Play();
-				break;
-			case eSoundName.death:
-				sfxCS[5].Play();
-				break;
-			case eSoundName.confirm:
-				sfxCS[6].Play();
-				break;
-			case eSoundName.deny:
-				sfxCS[7].Play();
-				break;
-			case eSoundName.run:
-				sfxCS[8].Play();
-				break;
-			case eSoundName.fireball:
-				sfxCS[9].Play();
-				break;
-			case eSoundName.fireblast:
-				sfxCS[10].Play();
-				break;
-			case eSoundName.buff1:
-				sfxCS[11].Play();
-				break;
-			case eSoundName.buff2:
-				sfxCS[12].Play();
-				break;
-			case eSoundName.highBeep1:
-				sfxCS[13].Play();
-				break;
-			case eSoundName.highBeep2:
-				sfxCS[14].Play();
-				break;
+			case eSoundName.dialogue: sfxCS[0].Play(); break;
+			case eSoundName.selection: sfxCS[1].Play(); break;
+			case eSoundName.damage1: sfxCS[2].Play(); break;
+			case eSoundName.damage2: sfxCS[3].Play(); break;
+			case eSoundName.damage3: sfxCS[4].Play(); break;
+			case eSoundName.death: sfxCS[5].Play(); break;
+			case eSoundName.confirm: sfxCS[6].Play(); break;
+			case eSoundName.deny: sfxCS[7].Play(); break;
+			case eSoundName.run: sfxCS[8].Play(); break;
+			case eSoundName.fireball: sfxCS[9].Play(); break;
+			case eSoundName.fireblast: sfxCS[10].Play(); break;
+			case eSoundName.buff1: sfxCS[11].Play(); break;
+			case eSoundName.buff2: sfxCS[12].Play(); break;
+			case eSoundName.highBeep1: sfxCS[13].Play(); break;
+			case eSoundName.highBeep2: sfxCS[14].Play(); break;
+		}
+	}
+
+	public void SetMasterVolume(float volume) {
+		AudioListener.volume = volume;
+	}
+
+	public void SetBGMVolume(float volume) {
+		for(int i = 0; i < bgmCS.Count; i++) {
+			bgmCS[i].volume = volume;
+        }
+	}
+
+	public void SetSFXVolume(float volume) {
+		for (int i = 0; i < sfxCS.Count; i++) {
+			sfxCS[i].volume = volume;
 		}
 	}
 }
