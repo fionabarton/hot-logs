6bf0f2e0c9ca531b1160485bd40b00f8cf1ced3b
fionabarton <56236345+fionabarton@users.noreply.github.com>
2021-10-31 19:48:41 -0400

    Constrain bug movement
    - Option to constrain movement to fly zone
    - Flip facing direction

--- _Scripts/BugMovement.cs
+++ _Scripts/BugMovement.cs
@@ -14,7 +14,13 @@ public class BugMovement : MonoBehaviour {
 	public Vector2			randomMoveLength = new Vector2(0.05f, 0.25f);
 	public Vector2			randomWaitLength = new Vector2(0.05f, 0.25f);
 
+	public Vector2			flyZone = new Vector2(2.5f, 2.5f);
+
 	public int				animationNdx;
+
+	public bool				isConstrainedToFlyZone;
+
+	public bool				facingRight;
 
 	[Header ("Set Dynamically")]
 	private Rigidbody2D		rigid;
@@ -29,12 +35,32 @@ public class BugMovement : MonoBehaviour {
 
 	private Vector3 		moveDirection;
 
+	// Bounds of fly zone
+	Vector2					minFlyPoint;
+	Vector2					maxFlyPoint;
+
+	void OnBecameVisible() {
+		canMove = true;
+
+		StartCoroutine("FixedUpdateCoroutine");
+	}
+	void OnBecameInvisible() {
+		canMove = false;
+		rigid.velocity = Vector2.zero;
+
+		StopCoroutine("FixedUpdateCoroutine");
+	}
+
 	void Start () {
 		// Set up Movement
 		rigid = GetComponent<Rigidbody2D> ();
 
 		moveLengthCounter = Random.Range (randomMoveLength.x, randomMoveLength.y);
 		waitLengthCounter = Random.Range (randomWaitLength.x, randomWaitLength.y);
+
+		// Set bounds of fly zone
+		minFlyPoint = new Vector2(transform.position.x - flyZone.x, transform.position.y - flyZone.y);
+		maxFlyPoint = new Vector2(transform.position.x + flyZone.x, transform.position.y + flyZone.y);
 
 		// Assign Sorting Layer
 		sRend = GetComponent<SpriteRenderer> ();
@@ -68,36 +94,53 @@ public class BugMovement : MonoBehaviour {
 
 	void Move(){
 		if (isMoving) {
-			rigid.velocity = moveDirection;
-			moveLengthCounter -= Time.deltaTime;
+            // If bug has flown out of fly zone...
+            if (isConstrainedToFlyZone) {
+				if (transform.position.x >= maxFlyPoint.x) {
+					isMoving = false;
+					waitLengthCounter = Random.Range(randomWaitLength.x, randomWaitLength.y);
+					moveDirection = Vector3.left * moveSpeed;
+				} else if (transform.position.y >= maxFlyPoint.y) {
+					isMoving = false;
+					waitLengthCounter = Random.Range(randomWaitLength.x, randomWaitLength.y);
+					moveDirection = Vector3.down * moveSpeed;
+				} else if (transform.position.x <= minFlyPoint.x) {
+					isMoving = false;
+					waitLengthCounter = Random.Range(randomWaitLength.x, randomWaitLength.y);
+					moveDirection = Vector3.right * moveSpeed;
+				} else if (transform.position.y <= minFlyPoint.y) {
+					isMoving = false;
+					waitLengthCounter = Random.Range(randomWaitLength.x, randomWaitLength.y);
+					moveDirection = Vector3.up * moveSpeed;
+				}
+			}
 
+			// If time is up...
 			if (moveLengthCounter <= 0) {
 				isMoving = false;
-				waitLengthCounter = Random.Range (randomWaitLength.x, randomWaitLength.y);
+				waitLengthCounter = Random.Range(randomWaitLength.x, randomWaitLength.y);
 				moveDirection = Random.onUnitSphere * moveSpeed;
 			}
+
+			// Flip
+			if ((rigid.velocity.x < 0 && facingRight) || (rigid.velocity.x > 0 && !facingRight)) {
+				Utilities.S.Flip(gameObject, ref facingRight);
+			}
+
+			// Move bug and decrement timer
+			rigid.velocity = moveDirection;
+			moveLengthCounter -= Time.deltaTime;
 		} else {
+			// Freeze bug position and decrement timer
 			rigid.velocity = Vector2.zero;
 			waitLengthCounter -= Time.deltaTime;
 
+			// If time is up...
 			if (waitLengthCounter <= 0) {
 				isMoving = true;
 				moveLengthCounter = Random.Range (randomMoveLength.x, randomMoveLength.y);
 				moveDirection = Random.onUnitSphere * moveSpeed;
 			}
 		}
-	}
-
-
-	void OnBecameVisible(){
-		canMove = true;
-
-		StartCoroutine ("FixedUpdateCoroutine");
-	}
-	void OnBecameInvisible(){
-		canMove = false;
-		rigid.velocity = Vector2.zero;
-
-		StopCoroutine ("FixedUpdateCoroutine");
 	}
 }
