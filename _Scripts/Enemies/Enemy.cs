6182b5bc22a77831c85549b5b481d087681c3796
fionabarton <56236345+fionabarton@users.noreply.github.com>
2021-11-02 18:26:48 -0400

    Add enemy "ground level" field
    - Add “level” field to track what ground level the enemy is currently on.
    - Only call StartBattle() if it is on the same level as the player.

--- _Scripts/Enemies/Enemy.cs
+++ _Scripts/Enemies/Enemy.cs
@@ -12,7 +12,10 @@ public class Enemy : MonoBehaviour {
 	[Header("Set in Inspector")]
 	public List<EnemyStats> stats;
 
-    [Header("Set Dynamically")]
+	// The ground level that the enemy is on
+	public int				level = 1;
+
+	[Header("Set Dynamically")]
 	// Used to track enemy in order to handle what happens if it's dead,
 	// defaultMovementMode has been changed, etc.
 	public int 				ndx;
@@ -24,35 +27,42 @@ public class Enemy : MonoBehaviour {
     // Start Battle
     void OnCollisionEnter2D(Collision2D coll){
 		if (coll.gameObject.CompareTag("Player")) {
-            if (!RPG.S.paused) {
-				// If this Enemy does not StartBattle() on collision
-				if (stats[0].questNdx == 0) {
-					if (!Player.S.invincibility.isInvincible && !Player.S.isBattling) {
-						// Set Camera to Enemy gameObject
-						CamManager.S.ChangeTarget(gameObject, true);
+			if (level == Player.S.level) {
+				if (!RPG.S.paused) {
+					// If this Enemy does not StartBattle() on collision
+					if (stats[0].questNdx == 0) {
+						if (!Player.S.invincibility.isInvincible && !Player.S.isBattling) {
+							// Set Camera to Enemy gameObject
+							CamManager.S.ChangeTarget(gameObject, true);
 
-						// Prevent collisions w/ multiple enemies
-						Player.S.isBattling = true;
+							// Prevent collisions w/ multiple enemies
+							Player.S.isBattling = true;
 
-						// Start Battle ( Assign Enemy Stats)
-						RPG.S.StartBattle(stats);
+							// Start Battle ( Assign Enemy Stats)
+							RPG.S.StartBattle(stats);
 
-						// Inform EnemyManager this Enemy has battled and is the current enemy
-						EnemyManager.S.enemiesBattled.Add(ndx);
-						EnemyManager.S.currentEnemyNdx = ndx;
-						// Store position of all enemies in currentScene
-						EnemyManager.S.PopulateEnemyPositionsList();
+							// Inform EnemyManager this Enemy has battled and is the current enemy
+							EnemyManager.S.enemiesBattled.Add(ndx);
+							EnemyManager.S.currentEnemyNdx = ndx;
 
-						EnemyMovement enemyMove = GetComponent<EnemyMovement>();
-						if (enemyMove != null) {
-							// Freeze Enemy
-							enemyMove.canMove = false;
+							// Store position of each enemy in currentScene
+							EnemyManager.S.GetEnemyPositions();
+							// Store level of each enemy in currentScene
+							EnemyManager.S.GetEnemyLevels();
+							// Store sorting layer name of each swap layer sprite in currentScene
+							SwapLayerManager.S.GetLayerNames();
+
+							EnemyMovement enemyMove = GetComponent<EnemyMovement>();
+							if (enemyMove != null) {
+								// Freeze Enemy
+								enemyMove.canMove = false;
+							}
+
+							// Get and position Explosion game object
+							GameObject explosion = ObjectPool.S.GetPooledObject("Explosion");
+							explosion.SetActive(true);
+							Utilities.S.SetPosition(explosion, coll.contacts[0].point.x, coll.contacts[0].point.y);
 						}
-
-						// Get and position Explosion game object
-						GameObject explosion = ObjectPool.S.GetPooledObject("Explosion");
-						explosion.SetActive(true);
-						Utilities.S.SetPosition(explosion, coll.contacts[0].point.x, coll.contacts[0].point.y);
 					}
 				}
 			}
