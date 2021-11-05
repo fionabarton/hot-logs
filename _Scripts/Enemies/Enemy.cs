ac2e441766edaa6a41ac0a0df2bfdefc86ea555f
fionabarton <56236345+fionabarton@users.noreply.github.com>
2021-11-05 16:53:15 -0400

    Add “isOnSwapLayerTrigger” field
    - If the player/enemy are both on a swap layer trigger, call StartBattle() OnCollision even if they're not on the same ground level

--- _Scripts/Enemies/Enemy.cs
+++ _Scripts/Enemies/Enemy.cs
@@ -15,56 +15,76 @@ public class Enemy : MonoBehaviour {
 	// The ground level that the enemy is on
 	public int				level = 1;
 
+	// Amount of enemies to battle. If 999, set to a random amount
+	public int				enemyAmount = 999;
+
 	[Header("Set Dynamically")]
 	// Used to track enemy in order to handle what happens if it's dead,
 	// defaultMovementMode has been changed, etc.
 	public int 				ndx;
 
+	// If player/enemy are both on a swap layer trigger,
+	// Call StartBattle() OnCollision even if they're not on the same ground level
+	public bool				isOnSwapLayerTrigger;
+
     private void Start() {
+		// Get the index of this enemy within the current scene's Enemies gameObject holder
 		ndx = transform.GetSiblingIndex();
 	}
 
     // Start Battle
     void OnCollisionEnter2D(Collision2D coll){
 		if (coll.gameObject.CompareTag("Player")) {
+			// If player/enemy are on the same ground level
 			if (level == Player.S.level) {
 				if (!RPG.S.paused) {
-					// If this Enemy does not StartBattle() on collision
-					if (stats[0].questNdx == 0) {
-						if (!Player.S.invincibility.isInvincible && !Player.S.isBattling) {
-							// Set Camera to Enemy gameObject
-							CamManager.S.ChangeTarget(gameObject, true);
-
-							// Prevent collisions w/ multiple enemies
-							Player.S.isBattling = true;
-
-							// Start Battle ( Assign Enemy Stats)
-							RPG.S.StartBattle(stats);
-
-							// Inform EnemyManager this Enemy has battled and is the current enemy
-							EnemyManager.S.enemiesBattled.Add(ndx);
-							EnemyManager.S.currentEnemyNdx = ndx;
-
-							// Store position of each enemy in currentScene
-							EnemyManager.S.GetEnemyPositions();
-							// Store level of each enemy in currentScene
-							EnemyManager.S.GetEnemyLevels();
-							// Store sorting layer name of each swap layer sprite in currentScene
-							SwapLayerManager.S.GetLayerNames();
-
-							EnemyMovement enemyMove = GetComponent<EnemyMovement>();
-							if (enemyMove != null) {
-								// Freeze Enemy
-								enemyMove.canMove = false;
-							}
-
-							// Get and position Explosion game object
-							GameObject explosion = ObjectPool.S.GetPooledObject("Explosion");
-							explosion.SetActive(true);
-							Utilities.S.SetPosition(explosion, coll.contacts[0].point.x, coll.contacts[0].point.y);
-						}
-					}
+					StartBattle(coll, enemyAmount);
 				}
+			} else {
+				// If player/enemy are both on a swap layer trigger
+				if (isOnSwapLayerTrigger && Player.S.isOnSwapLayerTrigger) {
+					StartBattle(coll, enemyAmount);
+				}
+            }
+		}
+	}
+
+	void StartBattle(Collision2D coll, int enemyAmount) {
+		// If this Enemy does not StartBattle() on collision
+		if (stats[0].questNdx == 0) {
+			if (!Player.S.invincibility.isInvincible && !Player.S.isBattling) {
+				// Set Camera to Enemy gameObject
+				CamManager.S.ChangeTarget(gameObject, true);
+
+				// Prevent collisions w/ multiple enemies
+				Player.S.isBattling = true;
+
+				// Start Battle ( Assign Enemy Stats)
+				RPG.S.StartBattle(stats, enemyAmount);
+
+				// Inform EnemyManager this Enemy has battled and is the current enemy
+				EnemyManager.S.enemiesBattled.Add(ndx);
+				EnemyManager.S.currentEnemyNdx = ndx;
+
+				// Store position of each enemy in currentScene
+				EnemyManager.S.GetEnemyPositions();
+				// Store level of each enemy in currentScene
+				EnemyManager.S.GetEnemyLevels();
+				// Store enemy amounts of each enemy in currentScene
+				EnemyManager.S.GetEnemyAmounts();
+				// Store sorting layer name of each swap layer sprite in currentScene
+				SwapLayerManager.S.GetLayerNames();
+
+				EnemyMovement enemyMove = GetComponent<EnemyMovement>();
+				if (enemyMove != null) {
+					// Freeze Enemy
+					enemyMove.canMove = false;
+				}
+
+				// Get and position Explosion game object
+				GameObject explosion = ObjectPool.S.GetPooledObject("Explosion");
+				explosion.SetActive(true);
+				Utilities.S.SetPosition(explosion, coll.contacts[0].point.x, coll.contacts[0].point.y);
 			}
 		}
 	}
