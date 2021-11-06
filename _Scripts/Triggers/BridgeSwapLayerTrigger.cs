f3bd66f4a9932bfcfd4778fa0fd0e1102871f0a2
fionabarton <56236345+fionabarton@users.noreply.github.com>
2021-11-06 11:07:02 -0400

    BridgeSwapLayerTrigger.cs: Add OnEnable()
    - In OnEnable(), set sorting layer names and enable colliders

--- _Scripts/Triggers/BridgeSwapLayerTrigger.cs
+++ _Scripts/Triggers/BridgeSwapLayerTrigger.cs
@@ -5,28 +5,55 @@ using UnityEngine;
 public class BridgeSwapLayerTrigger : MonoBehaviour {
 	[Header("Set in Inspector")]
     // Colliders that enable/disable when Player enters trigger
-    public GameObject enableCollider;
-    public GameObject disableCollider;
+    public GameObject       horizontalCollider;
+    public GameObject       verticalCollider;
 
     // Sprites that change sorting layer when Player enters trigger
-	public SpriteRenderer frontSprite;
-    public SpriteRenderer backSprite;
+	public SpriteRenderer   frontSprite;
+    public SpriteRenderer   backSprite;
 
-    public bool isHorizontalTrigger;
+    public bool             isHorizontalTrigger;
+
+    public int              level;
+
+    private void OnEnable() {
+        if(level == Player.S.level) {
+            // Set the sorting layer of each sprite
+            frontSprite.sortingLayerName = "Player";
+            backSprite.sortingLayerName = "Foreground";
+
+            //Enable/Disable colliders
+            horizontalCollider.SetActive(true);
+            verticalCollider.SetActive(false);
+        } else {
+            // Set the sorting layer of each sprite
+            frontSprite.sortingLayerName = "Player";
+            backSprite.sortingLayerName = "Player";
+
+            //Enable/Disable colliders
+            horizontalCollider.SetActive(false);
+            verticalCollider.SetActive(true);
+        }
+    }
 
     void OnTriggerEnter2D(Collider2D coll) {
         if (coll.gameObject.CompareTag("PlayerTrigger")) {
-            //Enable/Disable colliders
-            enableCollider.SetActive(true);
-            disableCollider.SetActive(false);
-
-            // Change the sorting layer of the sprites
 			if (isHorizontalTrigger) {
-				frontSprite.sortingLayerName = "Player";
+                // Set the sorting layer of each sprite
+                frontSprite.sortingLayerName = "Player";
                 backSprite.sortingLayerName = "Foreground";
-			} else {
+
+                //Enable/Disable colliders
+                horizontalCollider.SetActive(true);
+                verticalCollider.SetActive(false);
+            } else {
+                // Set the sorting layer of each sprite
                 frontSprite.sortingLayerName = "Player";
                 backSprite.sortingLayerName = "Player";
+
+                //Enable/Disable colliders
+                horizontalCollider.SetActive(false);
+                verticalCollider.SetActive(true);
             }
         }
 	}
