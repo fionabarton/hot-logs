51a75117a50e6424098ed9843439ba6d9f97d07b
fionabarton <56236345+fionabarton@users.noreply.github.com>
2021-11-14 14:28:08 -0500

    TitleScreen.cs: Prevent player input
    - Prevent player input
    - On new game, close and open battle curtain

--- _Scripts/UI/TitleScreen.cs
+++ _Scripts/UI/TitleScreen.cs
@@ -7,17 +7,113 @@ public class TitleScreen : MonoBehaviour{
     [Header("Set in Inspector")]
     public List<Button> buttons;
 
-    void Start(){
-        // New Game Button
-        buttons[0].onClick.AddListener(delegate { RPG.S.LoadLevel("Town_1"); });
+    [Header("Set Dynamically")]
+    // Singleton
+    private static TitleScreen _S;
+    public static TitleScreen S { get { return _S; } set { _S = value; } }
 
-        // Load Game Button
-        //buttons[1].onClick.AddListener(delegate { RPG.S.LoadLevel("Area_1"); });
+    // Allows parts of Loop() to be called once rather than repeatedly every frame.
+    public bool         canUpdate;
 
-        // Options Button
-        //buttons[2].onClick.AddListener(delegate { RPG.S.LoadLevel("Area_2"); });
+    // Ensures audio is only played once when button is selected
+    public GameObject   previousSelectedButton;
 
-        // Set Selected GameObject (New Game Button)
-        Utilities.S.SetSelectedGO(buttons[0].gameObject);
+    void Awake() {
+        S = this;
+    }
+
+    public void Activate() {
+        // Activate TitleScreenButtons gameObject
+        buttons[0].transform.parent.gameObject.SetActive(true);
+
+        // Add listeners
+        buttons[0].onClick.AddListener(NewGame);
+        buttons[1].onClick.AddListener(SaveScreen.S.Activate);
+        buttons[2].onClick.AddListener(OptionsScreen.S.Activate);
+
+        // Set Selected GameObject and Position Cursor 
+        if (PlayerPrefs.HasKey("0Time") || PlayerPrefs.HasKey("1Time") || PlayerPrefs.HasKey("2Time")) {
+            if (PlayerPrefs.GetString("0Time") == "0:00" && PlayerPrefs.GetString("1Time") == "0:00" && PlayerPrefs.GetString("2Time") == "0:00") {
+                // Set Selected GameObject: New Game Button
+                SetSelectedButton(0);
+            } else {
+                // Set Selected GameObject: Load Game Button
+                SetSelectedButton(0);
+            }
+        } else {
+            // Set Selected GameObject: New Game Button
+            SetSelectedButton(0);
+        } 
+
+        // Activate Cursor
+        ScreenCursor.S.cursorGO[0].SetActive(true);
+
+        // Prevent player input
+        Player.S.canMove = false;
+
+        // Add Loop() to Update Delgate
+        UpdateManager.updateDelegate += Loop;
+    }
+
+    void SetSelectedButton(int ndx) {
+        Utilities.S.SetSelectedGO(buttons[ndx].gameObject); 
+        Utilities.S.PositionCursor(buttons[ndx].gameObject, 150, -50);
+
+        // Set selected button text color	
+        buttons[ndx].gameObject.GetComponentInChildren<Text>().color = new Color32(205, 208, 0, 255);
+    }
+
+    public void Deactivate() {
+        // Deactivate TitleScreenButtons gameObject
+        buttons[0].transform.parent.gameObject.SetActive(false);
+
+        // Deactivate screen cursors
+        Utilities.S.SetActiveList(ScreenCursor.S.cursorGO, false);
+
+        // Update Delegate
+        UpdateManager.updateDelegate -= Loop;
+    }
+
+	public void Loop() {
+		// Reset canUpdate
+		if (Input.GetAxisRaw("Horizontal") != 0f || Input.GetAxisRaw("Vertical") != 0f) {
+			canUpdate = true;
+		}
+
+        if (canUpdate) {
+            if (!OptionsScreen.S.gameObject.activeInHierarchy && !SaveScreen.S.gameObject.activeInHierarchy) {
+                for (int i = 0; i < buttons.Count; i++) {
+                    if (UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject == buttons[i].gameObject) {
+                        // Set Cursor Position set to Selected Button
+                        Utilities.S.PositionCursor(buttons[i].gameObject, 150, -50);
+
+                        // Set selected button text color	
+                        buttons[i].gameObject.GetComponentInChildren<Text>().color = new Color32(205, 208, 0, 255);
+
+                        // Audio: Selection (when a new gameObject is selected)
+                        Utilities.S.PlayButtonSelectedSFX(ref previousSelectedButton);
+                    } else {
+                        // Set non-selected button text color
+                        buttons[i].gameObject.GetComponentInChildren<Text>().color = new Color32(255, 255, 255, 255);
+                    }
+                }
+
+            }
+        }
+    }
+
+    public void NewGame() {
+        // Close Curtains
+        BattleCurtain.S.Close();
+
+        // Audio: Buff 2
+        AudioManager.S.PlaySFX(eSoundName.buff2);
+
+        // Delay, then Load Scene
+        Invoke("LoadFirstScene", 1.25f);
+    }
+
+    void LoadFirstScene() {
+        RPG.S.LoadLevel("Area_2");
     }
 }
