3281d327c86c8ac96b4238ea3326f3ce513632e3
fionabarton <56236345+fionabarton@users.noreply.github.com>
2021-12-04 17:49:35 -0500

    Add party sleep “animation”

--- _Scripts/Triggers/InnkeeperTrigger.cs
+++ _Scripts/Triggers/InnkeeperTrigger.cs
@@ -9,7 +9,7 @@ public class InnkeeperTrigger : ActivateOnButtonPress {
 		CamManager.S.ChangeTarget(gameObject, true);
 
 		// Set SubMenu Text
-		SubMenu.S.SetText("Yes!", "No...");
+		RPG.S.gameSubMenu.SetText("Yes", "No");
 
 		DialogueManager.S.DisplayText("<color=yellow><Inn Keeper></color> Rooms are 10 gold a night. Restores your HP & MP. Would you like to spend the night?");
 
@@ -21,21 +21,19 @@ public class InnkeeperTrigger : ActivateOnButtonPress {
 		DialogueManager.S.grayOutTextBox = true;
 
 		// Set OnClick Methods
-		Utilities.S.RemoveListeners(SubMenu.S.buttonCS);
-		SubMenu.S.buttonCS[0].onClick.AddListener(Yes);
-        SubMenu.S.buttonCS[1].onClick.AddListener(No);
-		//SubMenu.S.subMenuButtonCS[2].onClick.AddListener(Option2);
-		//SubMenu.S.subMenuButtonCS[3].onClick.AddListener(Option3);
+		Utilities.S.RemoveListeners(RPG.S.gameSubMenu.buttonCS);
+		RPG.S.gameSubMenu.buttonCS[0].onClick.AddListener(Yes);
+        RPG.S.gameSubMenu.buttonCS[1].onClick.AddListener(No);
+		//RPG.S.gameSubMenu.subMenuButtonCS[2].onClick.AddListener(Option2);
+		//RPG.S.gameSubMenu.subMenuButtonCS[3].onClick.AddListener(Option3);
 
 		// Set button navigation
-		Utilities.S.SetButtonNavigation(SubMenu.S.buttonCS[0], SubMenu.S.buttonCS[1], SubMenu.S.buttonCS[1]);
-		Utilities.S.SetButtonNavigation(SubMenu.S.buttonCS[1], SubMenu.S.buttonCS[0], SubMenu.S.buttonCS[0]);
+		Utilities.S.SetVerticalButtonNavigation(RPG.S.gameSubMenu.buttonCS[0], RPG.S.gameSubMenu.buttonCS[1], RPG.S.gameSubMenu.buttonCS[1]);
+		Utilities.S.SetVerticalButtonNavigation(RPG.S.gameSubMenu.buttonCS[1], RPG.S.gameSubMenu.buttonCS[0], RPG.S.gameSubMenu.buttonCS[0]);
 	}
 
 	public void Yes() {
 		AudioManager.S.PlaySFX(eSoundName.confirm);
-
-		DialogueManager.S.ResetSubMenuSettings();
 
 		int price = 10;
 
@@ -44,23 +42,56 @@ public class InnkeeperTrigger : ActivateOnButtonPress {
 			Party.S.gold -= price;
 
 			// Max HP/MP
-			Party.stats[0].HP = Party.stats[0].maxHP;
-			Party.stats[0].MP = Party.stats[0].maxMP;
-			Party.stats[1].HP = Party.stats[1].maxHP;
-			Party.stats[1].MP = Party.stats[1].maxMP;
+			Party.S.stats[0].HP = Party.S.stats[0].maxHP;
+			Party.S.stats[0].MP = Party.S.stats[0].maxMP;
+			Party.S.stats[1].HP = Party.S.stats[1].maxHP;
+			Party.S.stats[1].MP = Party.S.stats[1].maxMP;
 
-			// Display Text: HP/MP Restored
-			DialogueManager.S.DisplayText("Health and magic restored. Bless your heart, babe!");
+			StartCoroutine("CloseCurtains");
 		} else {
 			// Display Text: Not enough Gold
 			DialogueManager.S.DisplayText("Begone with you, penniless fool! Waste not my worthless time!"); 
 		}
 	}
 
+	// 1) Freeze input, close curtains
+	IEnumerator CloseCurtains() {
+		// Deactivate player input
+		RPG.S.paused = true;
+
+		BattleCurtain.S.Close();
+
+		// Audio: Win
+		StartCoroutine(AudioManager.S.PlaySongThenResumePreviousSong(6));
+
+		yield return new WaitForSeconds(1.5f);
+		StartCoroutine("OpenCurtains");
+	}
+	// 2) Open curtains
+	IEnumerator OpenCurtains() {
+		DialogueManager.S.DeactivateTextBox();
+
+		BattleCurtain.S.Open();
+
+		yield return new WaitForSeconds(0.75f);
+		DisplayDialogue();
+	}
+	// 3) Dialogue, unfreeze input
+	void DisplayDialogue() {
+		// Audio: Buff 1
+		AudioManager.S.PlaySFX(eSoundName.buff1);
+
+		// Reactivate player input
+		RPG.S.paused = false;
+
+		DialogueManager.S.ResetSettings();
+		DialogueManager.S.DisplayText("Health and magic restored. Bless your heart, babe!");
+	}
+
 	public void No() {
 		AudioManager.S.PlaySFX(eSoundName.deny);
 
-		DialogueManager.S.ResetSubMenuSettings();
+		DialogueManager.S.ResetSettings();
 		DialogueManager.S.DisplayText("That's cool. Later, bro.");
 	}
 }
