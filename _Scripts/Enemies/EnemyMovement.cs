9f59ee6d9e4348c8b0888bd978525471bcc63c92
fionabarton <56236345+fionabarton@users.noreply.github.com>
2021-12-10 20:36:02 -0500

    Add MoveTowards enemy movement
    - Add MoveTowards() movement implementations (flee and pursue)
    - Refactor code

--- _Scripts/Enemies/EnemyMovement.cs
+++ _Scripts/Enemies/EnemyMovement.cs
@@ -8,19 +8,18 @@ using UnityEngine;
 /// </summary>
 public class EnemyMovement : MonoBehaviour {
 	[Header("Set in Inspector")]
-	public eMovement	currentMode = eMovement.random;
-	public eMovement	onForgetPlayerMode = eMovement.random;
+	public eMovement	currentMode = eMovement.randomWalk;
+	public eMovement	onForgetPlayerMode = eMovement.randomWalk;
 	public eMovement	onDetectPlayerMode;
 
 	public float		speed = 2;
 
 	public Vector2		walkZone = new Vector2(2.5f, 2.5f);
-
 	public Vector2		waitDuration = new Vector2(0.75f, 1.25f);
 
     [Header("Set Dynamically")]
 	// Components
-    private Rigidbody2D rigid;
+	public Rigidbody2D	rigid;
 	public Animator		anim;
 	private Enemy		enemy;
 
@@ -32,11 +31,10 @@ public class EnemyMovement : MonoBehaviour {
 
 	private bool		isWalking;
 	private int			walkDirection;
-	private int			nextWalkDirection = 999;
+	private int			nextWalkDirection = -1;
 	// 0 = right, 1 = up, 2 = left, 3 = down, 4 = Down Right, 5 = Up Right, 6 = Up Left, 7 = Down Left
 
 	private float		timer = 0;
-	//private float		waitCounter = 0;
 
 	// Flip
 	private bool		facingRight;
@@ -53,8 +51,8 @@ public class EnemyMovement : MonoBehaviour {
 		maxWalkPoint = new Vector2(transform.position.x + walkZone.x,
 								   transform.position.y + walkZone.y);
 
-		// Flee if the Player's level is double the Enemy's
-		if (Utilities.S.GetPercentage(enemy.stats[0].LVL, Party.stats[0].LVL) <= 0.5f) {
+		// Flee if the enemy's level is only 33% or less of the player's level 
+		if (Utilities.S.GetPercentage(enemy.stats[0].LVL, Party.S.stats[0].LVL) <= 0.33f) {
 			onDetectPlayerMode = eMovement.flee;
 		}
 
@@ -66,80 +64,32 @@ public class EnemyMovement : MonoBehaviour {
 		// if Visible
 		if (canMove) {
 			// if not Paused, no Dialogue Text Box
-			if (!RPG.S.paused && !DialogueManager.S.TextBoxSpriteGO.activeInHierarchy) {
+			if (!RPG.S.paused && !DialogueManager.S.textBoxSpriteGO.activeInHierarchy) {
 				// Flip
 				if ((rigid.velocity.x < 0 && !facingRight) || (rigid.velocity.x > 0 && facingRight)) {
 					Utilities.S.Flip(gameObject, ref facingRight);
 				}
 
 				switch (currentMode) {
-					case eMovement.random:
+					case eMovement.randomWalk:
 						// Move GameObject
 						if (isWalking) {
 							switch (walkDirection) {
-								case 0: //right
-									rigid.velocity = new Vector2(speed, 0);
-									if (transform.position.x >= maxWalkPoint.x) {
-										WaitSettings();
-										nextWalkDirection = 2;
-									}
-									break;
-								case 1: //up
-									rigid.velocity = new Vector2(0, speed);
-									if (transform.position.y >= maxWalkPoint.y) {
-										WaitSettings();
-										nextWalkDirection = 3;
-									}
-									break;
-								case 2: //left
-									rigid.velocity = new Vector2(-speed, 0);
-									if (transform.position.x <= minWalkPoint.x) {
-										WaitSettings();
-										nextWalkDirection = 0;
-									}
-									break;
-								case 3: //down
-									rigid.velocity = new Vector2(0, -speed);
-									if (transform.position.y <= minWalkPoint.y) {
-										WaitSettings();
-										nextWalkDirection = 1;
-									}
-									break;
-								case 4: //right down
-									rigid.velocity = new Vector2(speed / 2, -speed / 2);
-									if (transform.position.x >= maxWalkPoint.x || transform.position.y <= minWalkPoint.y) {
-										WaitSettings();
-										nextWalkDirection = 6;
-									}
-									break;
-								case 5: //right up
-									rigid.velocity = new Vector2(speed / 2, speed / 2);
-									if (transform.position.x >= maxWalkPoint.x || transform.position.y >= maxWalkPoint.y) {
-										WaitSettings();
-										nextWalkDirection = 7;
-									}
-									break;
-								case 6: //left up
-									rigid.velocity = new Vector2(-speed / 2, speed / 2);
-									if (transform.position.x <= minWalkPoint.x || transform.position.y >= maxWalkPoint.y) {
-										WaitSettings();
-										nextWalkDirection = 4;
-									}
-									break;
-								case 7: //left down
-									rigid.velocity = new Vector2(-speed / 2, -speed / 2);
-									if (transform.position.x <= minWalkPoint.x || transform.position.y <= minWalkPoint.y) {
-										WaitSettings();
-										nextWalkDirection = 5;
-									}
-									break;
+								case 0: Walk(speed, 0, 2); break; //right
+								case 1: Walk(0, speed, 3); break; //up
+								case 2: Walk(-speed, 0, 0); break; //left
+								case 3: Walk(0, -speed, 1); break; //down
+								case 4: Walk(speed / 2, -speed / 2, 6); break; //right down
+								case 5: Walk(speed / 2, speed / 2, 7); break; //right up
+								case 6: Walk(-speed / 2, speed / 2, 4); break; //left up
+								case 7: Walk(-speed / 2, -speed / 2, 5); break; //left down
 							}
 
 							// Count down Walk Counter
 							timer -= Time.deltaTime;
 
 							if (timer < 0) {
-								WaitSettings();
+								StartWaiting();
 							}
 						} else {
 							// Count down Wait Counter
@@ -147,7 +97,7 @@ public class EnemyMovement : MonoBehaviour {
 							rigid.velocity = Vector2.zero;
 
 							if (timer < 0) {
-								WalkSettings();
+								StartWalking();
 							}
 						}
 						break;
@@ -167,34 +117,20 @@ public class EnemyMovement : MonoBehaviour {
 						}
 						break;
 					case eMovement.pursueRun:
-						if (Player.S.gameObject.transform.position.x < transform.position.x && Player.S.gameObject.transform.position.y < transform.position.y) { // Left/Down
-							rigid.velocity = new Vector2(-speed, -speed);
-						} else if (Player.S.gameObject.transform.position.x < transform.position.x && Player.S.gameObject.transform.position.y > transform.position.y) { // Left/Up
-							rigid.velocity = new Vector2(-speed, speed);
-						} else if (Player.S.gameObject.transform.position.x > transform.position.x && Player.S.gameObject.transform.position.y < transform.position.y) { // Right/Down
-							rigid.velocity = new Vector2(speed, -speed);
-						} else if (Player.S.gameObject.transform.position.x > transform.position.x && Player.S.gameObject.transform.position.y > transform.position.y) { // Right/Up
-							rigid.velocity = new Vector2(speed, speed);
-						}
+						PursueOrFlee(gameObject, Player.S.gameObject);
+						//PursueOrFlee(gameObject, Player.S.gameObject, speed);
 						break;
 					case eMovement.flee:
-						if (Player.S.gameObject.transform.position.x > transform.position.x && Player.S.gameObject.transform.position.y > transform.position.y) { // Left/Down
-							rigid.velocity = new Vector2(-speed, -speed);
-						} else if (Player.S.gameObject.transform.position.x > transform.position.x && Player.S.gameObject.transform.position.y < transform.position.y) { // Left/Up
-							rigid.velocity = new Vector2(-speed, speed);
-						} else if (Player.S.gameObject.transform.position.x < transform.position.x && Player.S.gameObject.transform.position.y > transform.position.y) { // Right/Down
-							rigid.velocity = new Vector2(speed, -speed);
-						} else if (Player.S.gameObject.transform.position.x < transform.position.x && Player.S.gameObject.transform.position.y < transform.position.y) { // Right/Up
-							rigid.velocity = new Vector2(speed, speed);
-						}
+						PursueOrFlee(Player.S.gameObject, gameObject);
+						//PursueOrFlee(Player.S.gameObject, gameObject, speed * -1);
 						break;
 					case eMovement.reverse:
-						// Count Down back to defaultMovementMode
+						// Countdown back to defaultMovementMode
 						timer -= Time.deltaTime;
 						if (timer < 0) {
 							currentMode = cachedMode;
-							isWalking = true;
 							timer = Random.Range(waitDuration.x, waitDuration.y);
+							StartWalking();
 						}
 						break;
 				}
@@ -210,62 +146,109 @@ public class EnemyMovement : MonoBehaviour {
 	}
 
 	// Walk
-	public void WalkSettings() {
+	public void StartWalking() {
 		anim.speed = 1;
 		isWalking = true;
 		timer = Random.Range(waitDuration.x, waitDuration.y);
 
 		// Set walkDirection
-		if (nextWalkDirection == 999) {
+		if (nextWalkDirection == -1) {
 			// Select random direction
 			walkDirection = Random.Range(0, 8);
 		} else {
 			walkDirection = nextWalkDirection;
 			// Reset nextWalkDirection 
-			nextWalkDirection = 999;
+			nextWalkDirection = -1;
 		}
 	}
 
 	// Wait
-	public void WaitSettings() {
+	public void StartWaiting() {
 		anim.speed = 0;
-
 		isWalking = false;
 		timer = Random.Range(waitDuration.x, waitDuration.y);
 	}
 
-	// Bounce back in opposite direction (ex. collisions w/ walls, etc.)
+	void Walk(float xVelocity, float yVelocity, int nextDirection) {
+		rigid.velocity = new Vector2(xVelocity, yVelocity);
+		if (transform.position.x <= minWalkPoint.x || transform.position.x >= maxWalkPoint.x ||
+			transform.position.y <= minWalkPoint.y || transform.position.y >= maxWalkPoint.y) {
+			StartWaiting();
+			nextWalkDirection = nextDirection;
+		}
+	}
+
+	// My implementation
+	void PursueOrFlee(GameObject hunter, GameObject chase) {
+		if (hunter.transform.position.x >= chase.transform.position.x) {
+			if (hunter.transform.position.y >= chase.transform.position.y) {
+				rigid.velocity = new Vector2(-speed, -speed); // Left/Down
+			} else {
+				rigid.velocity = new Vector2(-speed, speed); // Left/Up
+			}
+		} else {
+			if (hunter.transform.position.y >= chase.transform.position.y) {
+				rigid.velocity = new Vector2(speed, -speed); // Right/Down
+			} else {
+				rigid.velocity = new Vector2(speed, speed); // Right/Up
+			}
+		}
+	}
+
+	// Uses Unity's MoveTowards function
+	void PursueOrFlee(GameObject hunter, GameObject chase, float speed) {
+		// Flip
+		if ((hunter.transform.position.x > chase.transform.position.x && !facingRight) ||
+			(hunter.transform.position.x < chase.transform.position.x && facingRight)) {
+			Utilities.S.Flip(gameObject, ref facingRight);
+		}
+
+		transform.position = Vector3.MoveTowards(transform.position, Player.S.transform.position, speed * Time.fixedDeltaTime);
+	}
+
+	// Bounce back in opposite direction 
 	void OnCollisionEnter2D(Collision2D coll) {
-		if (currentMode == eMovement.random || currentMode == eMovement.pursueWalk || currentMode == eMovement.pursueRun || currentMode == eMovement.flee) {
-			// If enemy is colliding with ONLY_PLAYER_ENEMY layer
-			if (coll.gameObject.layer == 28) {
-				// If not Paused, no Dialogue Text Box
-				if (!RPG.S.paused && !DialogueManager.S.TextBoxSpriteGO.activeInHierarchy) {
-					// Set timer duration
-					timer = 0.25f;
+		if (currentMode == eMovement.randomWalk) {
+			ChangeDirection(coll, false);
+		}
+	}
 
-					// Cache current mode
-					cachedMode = currentMode;
+	// Bounce back in random direction
+	void OnCollisionStay2D(Collision2D coll) {
+		if (currentMode == eMovement.pursueWalk || currentMode == eMovement.pursueRun || currentMode == eMovement.flee) {
+			ChangeDirection(coll, true);
+		}
+	}
 
-					// Set mode to Reverse
-					currentMode = eMovement.reverse;
+	void ChangeDirection(Collision2D coll, bool selectRandomDirection) {
+		// If enemy is colliding with ONLY_PLAYER_ENEMY layer
+		if (coll.gameObject.layer == 28) {
+			// If not Paused, no Dialogue Text Box
+			if (!RPG.S.paused && !DialogueManager.S.textBoxSpriteGO.activeInHierarchy) {
+				// Set timer duration
+				timer = 0.1f;
 
-					// Reverse Velocity
-					int randomNdx = Random.Range(0, 4);
-					switch (randomNdx) {
-						case 0:
-							rigid.velocity = new Vector2(-speed, -speed);
-							break;
-						case 1:
-							rigid.velocity = new Vector2(-speed, speed);
-							break;
-						case 2:
-							rigid.velocity = new Vector2(speed, -speed);
-							break;
-						case 3:
-							rigid.velocity = new Vector2(speed, speed);
-							break;
-					}
+				// Cache current mode
+				cachedMode = currentMode;
+
+				// Set mode to reverse
+				currentMode = eMovement.reverse;
+
+				// Randomly select direction
+				if (selectRandomDirection) {
+					walkDirection = Random.Range(0, 8);
+				}
+
+				// Go in opposite direction
+				switch (walkDirection) {
+					case 0: rigid.velocity = new Vector2(-speed, 0); break;
+					case 1: rigid.velocity = new Vector2(0, -speed); break;
+					case 2: rigid.velocity = new Vector2(speed, 0); break;
+					case 3: rigid.velocity = new Vector2(0, speed); break;
+					case 4: rigid.velocity = new Vector2(-speed / 2, speed / 2); break;
+					case 5: rigid.velocity = new Vector2(-speed / 2, -speed / 2); break;
+					case 6: rigid.velocity = new Vector2(speed / 2, -speed / 2); break;
+					case 7: rigid.velocity = new Vector2(speed / 2, speed / 2); break;
 				}
 			}
 		}
