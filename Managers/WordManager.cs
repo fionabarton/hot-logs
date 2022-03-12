657e664d235a810a4771b319cacb7d0546d9ff6c
fionabarton <56236345+fionabarton@users.noreply.github.com>
2022-03-12 17:56:41 -0500

    Add list of interjections

--- Managers/WordManager.cs
+++ Managers/WordManager.cs
@@ -8,6 +8,7 @@ using UnityEngine;
 public class WordManager : MonoBehaviour {
     [Header("Set Dynamically")]
     public List<string> exclamations = new List<string>();
+    public List<string> interjections = new List<string>();
 
     private static WordManager _S;
     public static WordManager S { get { return _S; } set { _S = value; } }
@@ -22,6 +23,12 @@ public class WordManager : MonoBehaviour {
             "Groovy", "Awesome", "Excellent", "Cool", "Incredible", "Unreal", "Fabulous", "Terrific", "Yay", 
             "Fantastic", "Great", "Gnarly", "Sweet", "Nice", "Splendid", "Wicked", "Wow", "Dude", "Cool beans",
             "Booyah", "Cowabunga", "Tubular" };
+
+        interjections = new List<string>() { "Rats", "Yuck", "Dang", "Darn", "Blast", "Oh bother", "Doggone it", "Darnation", 
+            "Gosh darn it", "Gosh darn it to heck", "Oh fiddlesticks", "Cripes", "Confound it", "Shucks", "Shoot", 
+            "Blooming heck", "Flipping heck", "Blinking heck", "Dash it", "Strike me pink", "My goodness", "For Pete's sake",
+            "For Heaven's sake", "Frick", "Good gosh", "Bloody heck", "Dagnabbit", "Oh poo","Blimey", "Great Scott", 
+            "Goodness me", "For crying out loud", "Good gracious", "Good golly", "Dang it", "Darn it" };
     }
 
     public string GetRandomExclamation() {
